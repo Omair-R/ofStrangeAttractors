133bac9daf6d2779af3914e9d8713080bdcb3a42
OMAIR\omair <omairalmanifi@gmail.com>
2025-02-11 23:03:23 +0800

    Reorganization of the main file

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include "ofMain.h"
-#include "ofApp.h"
 #include "ofPointSpaceApp3D.h"
 #include "ofPointSpaceApp2D.h"
 #include "ofHistogramApp.h"
@@ -10,65 +9,121 @@ int main( ){
 
 	//Use ofGLFWWindowSettings for more options like multi-monitor fullscreen
 	ofGLWindowSettings settings;
-	settings.setSize(1180, 820);
+	int window_width = 1180;
+	int window_height = 820;
+	settings.setSize(window_width, window_height);
 	settings.windowMode = OF_WINDOW; //can also be OF_FULLSCREEN
 
 	auto window = ofCreateWindow(settings);
 
-	std::shared_ptr<att::Gradient> gradient_test = std::make_shared< att::Gradient>(ofColor(147, 112, 219), ofColor(255, 218, 185));
+	//-----general artist and attractor settings.
+	int drawing_speed = 100;
+	int max_iters = 1'000'000;
+	int init_iters = 100;
+	att::OdeSolver ode_solver = att::OdeSolver::EULER;
+	ofPrimitiveMode primative_mode = OF_PRIMITIVE_POINTS;
+
+	ofColor background_color = ofColor::purple/5;
+	ofColor attractor_color = ofColor(181, 143, 206, 150);
+	std::shared_ptr<att::Gradient> gradient_test = std::make_shared< att::Gradient>(
+		ofColor(147, 112, 219), 
+		ofColor(255, 218, 185)
+	);
 	gradient_test->addColor(ofColor(200, 69, 69), 0.01);
 	gradient_test->addColor(ofColor(200, 200, 18), 0.1);
 
+	
 	// 3D Attractor ===========================================================================================
+	// 
+	 //std::array<att::real, 3> init_position{ 0.1f, 0.1f, 0.1f };
 	//std::shared_ptr<att::AbstractAttractor<3>> attractor = std::make_shared<att::LorenzAttractor>(0.01, true);
 	//std::shared_ptr<att::AbstractAttractor<3>> attractor = std::make_shared<att::ThomasSymmetricAttractor>(0.01, true);
 	//std::shared_ptr<att::AbstractAttractor<3>> attractor = std::make_shared<att::LangfordAttractor>(0.01, true);
-	//std::shared_ptr<att::AbstractAttractor<3>> attractor = std::make_shared<att::ChenAttractor>(0.01, true);
 	//std::shared_ptr<att::AbstractAttractor<3>> attractor = std::make_shared<att::DadrasAttractor>(0.01, true);
 	//std::shared_ptr<att::AbstractAttractor<3>> attractor = std::make_shared<att::HalvorsenAttractor>(0.01, true);
 	//std::shared_ptr<att::AbstractAttractor<3>> attractor = std::make_shared<att::RosslerAttractor>(0.01, true);
 	//std::shared_ptr<att::AbstractAttractor<3>> attractor = std::make_shared<att::RabinovichFabrikantAttractor>(0.01, true);
 	//std::shared_ptr<att::AbstractAttractor<3>> attractor = std::make_shared<att::SprottBAttractor>(0.05, true);
 
+	 //std::shared_ptr<att::AbstractAttractor<3>> attractor = std::make_shared<att::ChenAttractor>(0.01, true);
+
+	int particles = 10000;
+	std::array<att::real, 3> max_init_pos = { 1.0f, 1.0f, 1.0f };
+	std::array<att::real, 3> min_init_pos = { 0.0f, 0.0f, 0.0f };
+
 	/*ofRunApp(window, make_shared<ofParticlesApp>(
 		attractor,
-		10000,
-		ofColor(181, 143, 206, 150)
+		particles,
+		attractor_color,
+		max_iters,
+		max_init_pos,
+		min_init_pos,
+		drawing_speed,
+		init_iters,
+		background_color
 	));*/
 
 	/*ofRunApp(window, make_shared<ofPointSpaceApp3D>(
 	    attractor,
-		ofColor(181, 143, 206, 150)
+		attractor_color,
+		ode_solver,
+		primative_mode,
+		max_iters,
+		init_position,
+		drawing_speed,
+		init_iters,
+		background_color
 	));*/
 
 
 	// 2D Attractor ===========================================================================================
+	
+	std::array<att::real, 2> init_position{ 1.0f, 1.0f };
 	//std::shared_ptr<att::AbstractAttractor<2>> attractor = std::make_shared<att::CliffordAttractor>(1.2, false, 1.5, -1.8, 1.6, 0.9);
 	//std::shared_ptr<att::AbstractAttractor<2>> attractor = std::make_shared<att::PeterDeJongAttractor>(1.2, false);
 	//std::shared_ptr<att::AbstractAttractor<2>> attractor = std::make_shared<att::JohnnySvenssonAttractor>(1.2, false);
 	//std::shared_ptr<att::AbstractAttractor<2>> attractor = std::make_shared<att::GummowskiMiraAttractor>(1.2, false);
 	std::shared_ptr<att::AbstractAttractor<2>> attractor = std::make_shared<att::DuffingAttractor>(0.01, false);
 
-	//std::shared_ptr<att::AbstractAttractor<2>> attractor = std::make_shared<att::QuadraticSporttAttractor>(0.01, false);
+	//init_position = { -0.72f, -0.64f};
 	//std::shared_ptr<att::AbstractAttractor<2>> attractor = std::make_shared<att::TinkerbellAttractor>(1.2, false);
+
+	//init_position = { 0.01f, 0.01f }; //unstable for larger values.
 	//std::shared_ptr<att::AbstractAttractor<2>> attractor = std::make_shared<att::HenonMapBifurcationAttractor>(0.01, false);
+
+	//std::shared_ptr<att::AbstractAttractor<2>> attractor = std::make_shared<att::QuadraticSporttAttractor>(0.01, false);
+
+	
+	//---histogram specific settings.
+	std::array<int, 2> margins = { 50, 200 };
+	int attractor_width = window_width - margins[0] * 2;
+	int attractor_height = window_height - margins[1] * 2;
+	int blend_strength = 100;
 
 	ofRunApp(window, make_shared<ofHistogramApp>(
 		attractor,
-		1080,
-		720,
+		attractor_width,
+		attractor_height,
 		gradient_test,
-		std::array<int, 2> {50, 50},
-		1000000,
-		std::array<att::real, 2>{1.0, 1.0},
-		100,
-		ofColor::purple/5,
-		150
+		margins,
+		max_iters,
+		init_position,
+		init_iters,
+		background_color,
+		blend_strength
 	));
+
 
 	/*ofRunApp(window, make_shared<ofPointSpaceApp2D>(
 		attractor,
-		ofColor(181, 143, 206, 150)
+		attractor_color,
+		ode_solver,
+		primative_mode, 
+		max_iters,
+		init_position,
+		drawing_speed,
+		init_iters,
+		background_color
 	));*/
 
 	ofRunMainLoop();
